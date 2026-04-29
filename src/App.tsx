c6347fba0971c4b97c8f981fdff3b834b1d44849
Olabanji <emmanuelojo291@gmail.com>
2026-04-29 12:42:07 +0100

    error fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useWhisper } from './hooks/useWhisper';
 import { parseMeasurements, MEASUREMENT_PARTS } from './utils/parser';
 import { RecordingButton } from './components/RecordingButton';
